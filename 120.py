cf0d8df425fc364e0e851a1115357f039e2729e4
xydcz <510946106@qq.com>
2020-10-27 12:51:06 +0800

    '三角形最小路径和'

--- 120.py
+++ 120.py
@@ -1,10 +1,12 @@
 def trace(num):
-    s = 0
-    for i in num:
-        m = min(i)
-        s += m
-    print(s)
+    dp = [[0]*len(num[-1])] * len(num)
+    dp[-1] = num[-1]
+    for i in range(len(num)-2, -1, -1):
+        for j in range(len(num[i])):
+            dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + num[i][j]
+    return dp[0][0]
 
 
 num = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
-trace(num)
+a = trace(num)
+print(a)
