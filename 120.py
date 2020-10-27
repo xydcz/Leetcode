8c9a08b3e1d0b54f5e51c43f5878d06bcfb3a826
xydcz <510946106@qq.com>
2020-10-27 13:01:54 +0800

    优化空间复杂度

--- 120.py
+++ 120.py
@@ -1,4 +1,5 @@
-def trace(num):
+# 空间复杂度O(n*m)
+def trace_nm(num):
     dp = [[0]*len(num[-1])] * len(num)
     dp[-1] = num[-1]
     for i in range(len(num)-2, -1, -1):
@@ -7,6 +8,15 @@ def trace(num):
     return dp[0][0]
 
 
+# 空间复杂度O(n)
+def trace_n(num):
+    dp = num[-1]
+    for i in range(len(num)-2, -1, -1):
+        for j in range(len(num[i])):
+            dp[j] = num[i][j] + min(dp[j], dp[j+1])
+    return dp[0]
+
+
 num = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
-a = trace(num)
-print(a)
+print(trace_nm(num))
+print(trace_n(num))
